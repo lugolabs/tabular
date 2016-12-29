befd1416547137f9c1c9fbb7327bfbc855aa85f6
Artan Sinani <artisinani@gmail.com>
2016-12-29 11:58:07 +0100

    test model sending metadata

--- specs/src/model_spec.js
+++ specs/src/model_spec.js
@@ -1,6 +1,7 @@
 describe('tabular.Model', function() {
   describe('fetching', function() {
-    var options      = { source: '/data.json' },
+    var metadata     = { page: 2 },
+        options      = { source: '/data.json' },
         responseData = { metadata: {}, data: [] },
         element, response, model, server;
 
@@ -15,15 +16,13 @@ describe('tabular.Model', function() {
       element.remove();
     });
 
-    it('fetches data from server and triggers stopFetch event', function() {
+    it('fetches data from server sending metadata and triggers stopFetch event', function() {
       fetchFromServerAndTrigger('stopFetch');
-
       chai.assert.deepEqual(responseData, response);
     });
 
-    it('fetches data from server and triggers success event', function() {
+    it('fetches data from server sending metadata and triggers success event', function() {
       fetchFromServerAndTrigger('success');
-
       chai.assert.deepEqual(responseData, response);
     });
 
@@ -32,13 +31,15 @@ describe('tabular.Model', function() {
         response = resp;
       });
 
-      element.trigger('model:fetch');
-
-      server.requests[0].respond(
+      var url = options.source + '?' + $.param(metadata);
+      server.respondWith('GET', url, [
         200,
         { "Content-Type": "application/json" },
         JSON.stringify([responseData])
-      );
+      ]);
+
+      element.trigger('model:fetch', metadata);
+      server.respond();
     }
   });
 });
