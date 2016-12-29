191d4c45e9cd1452ac22e7bec66fba7f3e1a8940
Artan Sinani <artisinani@gmail.com>
2016-12-30 00:02:23 +0100

    create plugins on start

--- build/tabular.js
+++ build/tabular.js
@@ -5,8 +5,27 @@ tabular.VERSION = '0.1.0';
 
 $.extend(tabular, {
   start: function(element, options) {
+    var defaults = {
+      addHeading: function(el, tbl, opts) {
+        new tabular.Sort(el, tbl, opts);
+      },
+      plugins: [
+        'Model',
+        'Pagination',
+        'Search',
+        'Loader'
+      ]
+    };
+
+    options = $.extend({}, defaults, options);
+
     var jElement = $(element),
       view = new tabular.View(jElement, options);
+
+    $.map(options.plugins, function(plugin) {
+      var pluginClass = typeof plugin === 'string' ? tabular[plugin] : plugin;
+      new pluginClass(jElement, options);
+    });
 
     return jElement;
   }
--- specs/src/main_spec.js
+++ specs/src/main_spec.js
@@ -1,18 +1,101 @@
 describe('tabular', function() {
-  describe('#start', function() {
-    var element;
+  describe('start', function() {
+    var columns = [{ name: 'name', title: 'Name' }],
+      element, newElement;
 
     beforeEach(function() {
       element = $('<div id="tabular"/>');
     });
 
     afterEach(function() {
+      newElement.remove();
       element.remove();
     });
 
     it('should return element', function() {
-      var newElement = tabular.start(element[0], { columns: [] });
+      newElement = tabular.start(element[0], { columns: columns });
       chai.assert.equal(element.prop('id'), newElement.prop('id'));
+    });
+
+    it('renders correctly with default plugins', function() {
+      newElement = tabular.start(element[0], { columns: columns });
+      var markup = [
+        '<form class="tabular-search">',
+          '<input type="search" name="q">',
+        '</form>',
+        '<table>',
+          '<thead>',
+            '<tr>',
+              '<th class="tabular-sorting">',
+                'Name',
+                '<button data-sort="asc" data-column="name" class="tabular-sort"></button>',
+              '</th>',
+            '</tr>',
+          '</thead>',
+          '<tbody></tbody>',
+        '</table>',
+        '<div class="tabular-paginator">',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="first" disabled="disabled">First</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="prev" disabled="disabled">Previous</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="next" disabled="disabled">Next</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="last" disabled="disabled">Last</button>',
+        '</div>',
+        '<div class="tabular-loader">Loading ...</div>'
+      ].join('');
+      chai.assert.equal(markup, element.html());
+    });
+
+    it("doesn't render the plugins not included", function() {
+      newElement = tabular.start(element[0], {
+        columns: columns,
+        plugins: [
+          'Model',
+          'Loader'
+        ]
+      });
+      var markup = [
+        '<table>',
+          '<thead>',
+            '<tr>',
+              '<th class="tabular-sorting">',
+                'Name',
+                '<button data-sort="asc" data-column="name" class="tabular-sort"></button>',
+              '</th>',
+            '</tr>',
+          '</thead>',
+          '<tbody></tbody>',
+        '</table>',
+        '<div class="tabular-loader">Loading ...</div>'
+      ].join('');
+      chai.assert.equal(markup, element.html());
+    });
+
+    it("doesn't render sorting if not included", function() {
+      newElement = tabular.start(element[0], {
+        columns:    columns,
+        addHeading: null
+      });
+      var markup = [
+        '<form class="tabular-search">',
+          '<input type="search" name="q">',
+        '</form>',
+        '<table>',
+          '<thead>',
+            '<tr>',
+              '<th>Name</th>',
+            '</tr>',
+          '</thead>',
+          '<tbody></tbody>',
+        '</table>',
+        '<div class="tabular-paginator">',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="first" disabled="disabled">First</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="prev" disabled="disabled">Previous</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="next" disabled="disabled">Next</button>',
+          '<button type="button" class="tabular-btn tabular-pagination-btn" data-action="last" disabled="disabled">Last</button>',
+        '</div>',
+        '<div class="tabular-loader">Loading ...</div>'
+      ].join('');
+      chai.assert.equal(markup, element.html());
     });
   });
 });
